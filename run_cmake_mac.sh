f6a0ae581be55b70858999e03fa7e623e7d3106b
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2020-02-26 10:19:03 +0100

    Implement run_cmake_mac.sh script

---
+++ run_cmake_mac.sh
@@ -0,0 +1,29 @@
+#/bin/sh
+
+cmake_conda() {
+
+    if [ -z $CONDA_PREFIX ]
+    then
+        echo 'CONDA_PREFIX was not defined'
+        CONDA_PREFIX=$(dirname $(dirname $(which python)))
+        echo "CONDA_PREFIX is set to $CONDA_PREFIX"
+    else
+        echo "CONDA_PREFIX is $CONDA_PREFIX"
+    fi
+
+    PY_LIB=$(ls $CONDA_PREFIX/lib/libpython?.?m.dylib)
+    echo "Python library is at $PY_LIB"
+
+    cmake -DPYTHON_LIBRARY=$PY_LIB -DPYTHON_EXECUTABLE=$CONDA_PREFIX/bin/python ..
+
+} 
+
+if [[ $(which conda) ]]
+then
+    echo "conda is present"
+    cmake_conda
+
+else
+    echo "conda is absent"
+    cmake ..
+fi
